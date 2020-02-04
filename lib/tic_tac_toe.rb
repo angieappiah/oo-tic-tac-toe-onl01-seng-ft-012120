4405194dc81cf0f84fd630598bdfff091a234969
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-04 02:41:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,3 +3,12 @@ class song
     @board = board
   end
   
+  def 
+    board = [ 9 " "]
+    @board.new = []
+  end
+  
+  def Win_combination
+    Win_combination = [ "0"...."9"]
+    
+    def display_board
