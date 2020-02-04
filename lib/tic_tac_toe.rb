d8051b11237da5af9ffd8f8afb7c5beb34d7c034
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-04 02:57:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,7 +7,17 @@ class Tic_tac_toe
     @board = ( 9 " ")
   end
   
-  def Win_combination
-    Win_combination = 
+  def WIN_COMBINATIONS
+    WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+end
     
     def display_board
